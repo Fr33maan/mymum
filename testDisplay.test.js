41199b0b5b72bb0f4dafcd9a4a8059b7c19deb47
Fr33maan <jbbriant44@gmail.com>
2019-12-15 02:48:44 +0700

    FIX - use async/await everywhere and remove useless imports

--- testDisplay.test.js
+++ testDisplay.test.js
@@ -1,20 +1,22 @@
-const superagent = require("superagent");
-const express = require("express");
-const pgp = require("pg-promise")();
-const app = express();
-const db = pgp("postgres://postgres:9998@localhost:5432/my_database_test");
-const bodyParser = require("body-parser");
+const superagent = require("superagent")
+const pgp = require("pg-promise")()
+const db = pgp("postgres://postgres:9998@localhost:5432/my_database_test")
+// TODO import the server from server.js file
 
-beforeAll( () =>{
+// We empty the test database
+// TODO - start the server
+beforeAll( async () =>{
+  await db.any(`DELETE FROM Token`)
+  await db.any(`INSERT INTO Token(Type, Token) VALUES($1,$2)`,  ['digitalocean', 'abc'])
+  await db.any(`INSERT INTO Token(Type, Token) VALUES($1,$2)`,  ['azure', 'def'])
+  await db.any(`INSERT INTO Token(Type, Token) VALUES($1,$2)`,  ['gitlab', 'ghi'])
+})
 
-db.any(`DELETE FROM Token`).then()
-db.any(`INSERT INTO Token(Type, Token) VALUES($1,$2)`,  ['digitalocean', 'abc']).then()
-db.any(`INSERT INTO Token(Type, Token) VALUES($1,$2)`,  ['azure', 'def']).then()
-db.any(`INSERT INTO Token(Type, Token) VALUES($1,$2)`,  ['gitlab', 'ghi']).then()
-
-});
-
-test("On devrait recevoir la liste de tout les tokens", async (req, res) => {
-  const result = await (await superagent.get('/token')).body
-  expect(result).toEqual([{"token": "abc", "type": "digitalocean"}, {"token": "def", "type": "azure"}, {"token": "ghi", "type": "gitlab"}]); 
+test("should receive the list of tokens", async () => {
+  const { body } = await superagent.get('/tokens')
+  expect(result).toEqual([
+    {"token": "abc", "type": "digitalocean"}, 
+    {"token": "def", "type": "azure"}, 
+    {"token": "ghi", "type": "gitlab"}
+  ])
 })
