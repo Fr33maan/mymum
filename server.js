91dc0d81f865dcd731278658a31e21cf9f84db36
Mehdi Zenasni <mehdi.zenasni.pro@gmail.com>
2020-02-09 17:09:39 +0100

    Test signup réussi

--- server.js
+++ server.js
@@ -3,47 +3,44 @@ const app = express();
 const pgp = require("pg-promise")();
 const db = pgp("postgres://postgres:9998@localhost:5432/my_database");
 const bodyParser = require("body-parser");
+const http = require("http");
+var server;
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 function getTokens({ _db }) {
   return _db.any("SELECT * FROM token");
 }
 
-function getAccounts({_db}){
+function getAccounts({ _db }) {
   return _db.any("SELECT * FROM account");
 }
 
-async function insertTokens(type, token) {  
+async function insertTokens(type, token, { _db }) {
   try {
     const sql = `INSERT INTO token(type, token) VALUES($1,$2)`;
-    const users = await db.any(sql, [type, token]);
-         console.log("Token inséré dans la base de donnée");
+    const users = await _db.any(sql, [type, token]);
+    console.log("Token inséré dans la base de donnée");
   } catch (e) {
     console.log("Erreur :" + e);
   }
 }
 
-async function insertAccount(username, password, email){
-  try{
-
+async function insertAccount(username, password, email, { _db }) {
+  try {
     const sql = `INSERT INTO account(email, password, username) VALUES($1,$2,$3)`;
-    const users = await db.any(sql, [username, password, email]);
-      console.log("Utilisateur ajouté à la base de donnée");
-
-  }
-  catch(e){
-    console.log("Erreur : "+e);
+    const users = await _db.any(sql, [email, password, username]);
+    console.log("Utilisateur ajouté à la base de donnée");
+  } catch (e) {
+    console.log("Erreur : " + e);
   }
 }
 
-async function startServer({ _db = db }) {
+function startServer({ _db }) {
   app.get("/tokens", async (req, res, next) => {
     try {
       const tokens = await getTokens({ _db });
-
       res.send(tokens);
     } catch (e) {
       console.error(e);
@@ -61,35 +58,51 @@ async function startServer({ _db = db }) {
     }
   });
 
-  app.post("/tokens", (req, res) => {
+  app.post("/tokens", async (req, res) => {
     const type = req.body.type;
     const token = req.body.token;
 
     if (typeof type === "string" && typeof token === "number") {
-      insertTokens(type, token);
+      await insertTokens(type, token, { _db });
     } else {
       console.log("Erreur dans les types des données reçu");
     }
+    res.send({ status: "success" });
   });
 
-  app.post("/account", (req, res) => {
-
+  app.post("/account", async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     const email = req.body.email;
 
     if (username && password && email) {
-      insertAccount(username, password, email);
+      await insertAccount(username, password, email, { _db });
     } else {
       console.log("Utilisateur invalide");
     }
+
+    res.send({ status: "success" });
   });
 
-  app.listen(8080);
+  server = http.createServer(app).listen(8080);
 }
+
+function closeServer() {
+  server.close(() => console.log("closing server..."));
+}
+
+// async function bootstrap() {
+//   await startServer({_db : db});
+// }
+
+// bootstrap().then(() => console.log('Running'))
 
 module.exports = {
   startServer,
   getTokens,
-  getAccounts
+  getAccounts,
+  insertAccount,
+  insertTokens,
+  closeServer,
+  server
 };
--- tests/signup.test.js
+++ tests/signup.test.js
@@ -1,28 +1,33 @@
-require('iconv-lite/encodings');
-const superagent = require("superagent")
-const pgp = require("pg-promise")()
-const { Pool, Client } = require('pg')
-const db = pgp("postgres://postgres:9998@localhost:5432/my_database_test")
-const { startServer } = require('../server.js')
-
-
-var id
+const superagent = require("superagent");
+const pgp = require("pg-promise")();
+const db = pgp("postgres://postgres:9998@localhost:5432/my_database_test");
+const { startServer } = require("../server.js");
+const { closeServer } = require("../server.js");
 
 // We empty the test database and start the server
-beforeAll( async () =>{
-  await startServer({ _db: db })
-  const req = await superagent.get('http://localhost:8080/account')
-  id = req.body[0].id_user + 1;
-  await db.any(`DELETE FROM account`)
-  await superagent.post('http://localhost:8080/account')
-  .send({ email : "mehdi.zenasni.pro@gmail.com", password : "password1234", username : "zenasni" })
-  
-})
+
+beforeAll(async () => {
+  startServer({ _db: db });
+  await db.any(`TRUNCATE TABLE account RESTART IDENTITY`);
+  await superagent.post("http://localhost:8080/account").send({
+    email: "mehdi@gmail.com",
+    password: "password123",
+    username: "webizz"
+  });
+});
+
+afterAll(() => {
+  process.exit(0);
+});
 
 test("should receive the account created", async () => {
-  const { body } = await superagent.get('http://localhost:8080/account')
-  expect({body}).toEqual({"body": [
-    {"username" : "zenasni", "password": "password1234", "id_user" : id, "email": "mehdi.zenasni.pro@gmail.com"}
-  ]
-})
+  const { body } = await superagent.get("http://localhost:8080/account");
+  expect(body).toStrictEqual([
+    {
+      username: "webizz",
+      password: "password123",
+      id_user: 1,
+      email: "mehdi@gmail.com"
+    }
+  ]);
 });
